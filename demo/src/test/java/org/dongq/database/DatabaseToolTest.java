e64aa79422e1f66c401d335f6177392f42498eff
eastseven <eastseven@foxmail.com>
2012-10-30 18:06:55 +0800

    新增方法的测试

--- demo/src/test/java/org/dongq/database/DatabaseToolTest.java
+++ demo/src/test/java/org/dongq/database/DatabaseToolTest.java
@@ -1,8 +1,14 @@
 package org.dongq.database;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,30 +20,72 @@ public class DatabaseToolTest {
 
 	private static final Log log = LogFactory.getLog(DatabaseToolTest.class);
 	
-	private Table table;
+	private Table table = null;
 	
 	private ExportTool tool = new ExportTool();
 	
+	private List<Table> tables = null;
+	
 	@Before
 	public void init() {
-		List<Table> tables = tool.getTables();
+		tool.mkdir(ExportTool.sqlScriptDir);
+		tables = tool.getTables();
 		table = tables.get(new Random().nextInt(tables.size()));
+	}
+
+	@Test
+	public void testGetSequences() {
+		List<Sequence> sequences = tool.getSequences();
+		Assert.assertTrue(CollectionUtils.isNotEmpty(sequences));
+		for (Sequence sequence : sequences) {
+			System.out.println(sequence.getScript());
+		}
+	}
+	
+	@Test
+	public void printDatabaseInfo() throws Exception {
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		Connection conn = new ConnectionFactory().getConnect();
+		DatabaseMetaData dmd = conn.getMetaData();
+		
+		rs = dmd.getTableTypes();
+		while(rs.next()) {
+			System.out.println(rs.getString("TABLE_TYPE"));
+		}
+		
+		DbUtils.closeQuietly(conn, stmt, rs);
 	}
 	
 	@Test
 	public void testExportData() {
-		tool.exportData();
+		Assert.assertTrue(CollectionUtils.isNotEmpty(tables));
+		tool.exportData(tables);
 	}
 	
 	@Test
 	public void testExportTable() {
-		tool.exportTable();
+		Assert.assertTrue(CollectionUtils.isNotEmpty(tables));
+		tool.exportTable(tables);
 	}
 	
 	@Test
 	public void testCreateTableScript() {
+		Assert.assertNotNull(table);
 		String script = tool.createTableScript(table);
 		Assert.assertTrue(StringUtils.isNotBlank(script));
 		log.info(script);
 	}
+	
+	@Test
+	public void testCreateDatabaseScript() {
+		String username = "quickride";
+		String password = "201210301234";
+		String script = tool.generateCreateDatabaseScript(username, password);
+		
+		tool.generateCreateDatabaseScriptFile(script);
+		Assert.assertTrue(StringUtils.isNotBlank(script));
+		
+	}
+	
 }
