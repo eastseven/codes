88f5552a8492fa157577c34a5f80e453ee1b634d
eastseven <eastseven@foxmail.com>
2012-11-26 12:02:18 +0800

    update oracle_backup.sh

--- demo/shell-script/oracle_backup.sh
+++ demo/shell-script/oracle_backup.sh
@@ -7,3 +7,11 @@ echo $var_date
 exp quickrideproduct/quickrideproduct file=quickrideproduct-$var_date.dmp owner=quickrideproduct
 
 echo 'database backup done...'
+
+zip quickrideproduct$var_date.zip quickrideproduct-$var_date.dmp
+
+echo 'zip database backup done...'
+
+scp -P 2222 -r quickrideproduct$var_date.zip root@183.221.125.150:/root/quickrideproduct
+
+echo 'transfor the database file to si chuan 100 server...'
